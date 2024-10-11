0303e2001a2b016bc914cf6c1f939eae4adc09d0
GBergatto <gb.bergatto@gmail.com>
2024-10-11 18:21:12 +0200

    Add sketch for Serial relay

---
+++ SerialRelay/SerialRelay.ino
@@ -0,0 +1,18 @@
+void setup() {
+  Serial.begin(9600);  // Serial communication with the laptop
+  Serial1.begin(9600);  // Serial communication with the LoRa module
+}
+
+void loop() {
+  // If data is available from the LoRa module (Serial1), send it to the Serial Monitor
+  if (Serial1.available()) {
+    char data = Serial1.read();
+    Serial.write(data);  // Relay to Serial Monitor
+  }
+
+  // If data is available from the Serial Monitor (USB-C), send it to the LoRa module
+  if (Serial.available()) {
+    char data = Serial.read();
+    Serial1.write(data);  // Relay to LoRa module
+  }
+}
